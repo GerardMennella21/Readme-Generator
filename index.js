664f8b2deaa5c1e5326f50620c2ca47c06153d41
Gerard Mennella <gerardmennella21@gmail.com>
2022-04-20 12:57:11 -0400

    added inqirer questions

--- index.js
+++ index.js
@@ -1,12 +1,123 @@
 // TODO: Include packages needed for this application
 const fs = require("fs")
-const inquirer = require("inquirer")
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'Enter the title of your project.',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true
+                } else {
+                    console.log('Please enter the title of your project!')
+                    return false
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Enter a description of your project.',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true
+                } else {
+                    console.log('Please enter a description of your project!')
+                    return false
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'install',
+            message: 'Enter instructions to install your project.',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true
+                } else {
+                    console.log('Please enter instructions to install your project!')
+                    return false
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Enter usage information for your project.',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true
+                } else {
+                    console.log('Please enter usage information for your project!')
+                    return false
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'Enter the command used to run tests',
+            default: 'npm run test'
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Choose the projects license type',
+            choices: [
+
+            ]
+
+        },
+        {
+            type: 'input',
+            name: 'contributors',
+            message: 'Enter information regarding contributing to your project.',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true
+                } else {
+                    console.log('Please enter information regarding contributing to your project!')
+                    return false
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub username.',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true
+                } else {
+                    console.log('Please enter your GitHub username!')
+                    return false
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter your email address.',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true
+                } else {
+                    console.log('Please enter your email address!')
+                    return false
+                }
+            }
+        }
+    ])
+}
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
 
 // TODO: Create a function to initialize app
 function init() {}
